8ec52bec0ee40b2868581d88c77152e03f01f7a0
Matheus Gabriel Valenga <theusgvalenga@outlook.com>
2024-07-23 23:03:50 -0300

    feat: add see the exercises button
    Button that opens the drawer to see all routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import Button from '@/components/Button';
 import { Colors } from '@/constants/Colors';
 import { globalStyles } from '@/theme/styles';
 import { Ionicons } from '@expo/vector-icons';
-import { Link } from 'expo-router';
+import { DrawerActions } from '@react-navigation/native';
+import { Link, useNavigation } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
+  const navigation = useNavigation();
   return (
     <View style={globalStyles.centeredScreen}>
       <Ionicons name="logo-react" size={100} color={Colors.primary} />
@@ -22,6 +25,13 @@ export default function Index() {
           <Text style={styles.text}>See the repository</Text>
         </View>
       </Link>
+
+      <Button
+        style={{ marginTop: 44 }}
+        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+      >
+        <Button.Text>See the exercises</Button.Text>
+      </Button>
     </View>
   );
 }
